4829aeeb0ac29b1d7e2cc5af7ee15a88e73d8c6b
Chintan Shah <chnsh24@gmail.com>
2020-12-02 18:56:41 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dvrl',
+    version='',
+    packages=['dvrl', 'dvrl.data', 'dvrl.utils', 'dvrl.training', 'dvrl.validation', 'dvrl.visualization'],
+    url='',
+    license='',
+    author='chintanshah',
+    author_email='',
+    description=''
+)
